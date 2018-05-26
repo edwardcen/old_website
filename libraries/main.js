7d8740ee3fa1eb27a3e830ff898a03db3a535977
Edward Cen <edward.cen11@gmail.com>
2018-05-26 15:11:04 -0500

    changed two sigma to current intern

--- libraries/main.js
+++ libraries/main.js
@@ -3,6 +3,16 @@ $(".about").click(function() {
         $("#about-container").fadeIn('slow');
     });
 });
+$(".projects").click(function() {
+	$("#home-container").fadeOut('fast', function() {
+        $("#project-container").fadeIn('slow');
+    });
+});
+$(".home1").click(function() {
+	$("#project-container").fadeOut('fast', function() {
+        $("#home-container").fadeIn('slow');
+    });
+});
 $(".home").click(function() {
 	$("#about-container").fadeOut('fast', function() {
         $("#home-container").fadeIn('slow');
